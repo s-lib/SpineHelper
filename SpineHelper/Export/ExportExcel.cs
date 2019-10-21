55a6fe9ffdc2636d27a32102f83d95b0efa0c1ae
s-lib <s.libura@gmail.com>
2019-10-21 07:19:07 +0200

    Remove Office Interloop and implement OpenXml support

--- SpineHelper/Export/ExportExcel.cs
+++ SpineHelper/Export/ExportExcel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
-using Microsoft.Office.Interop.Excel;
+using System.IO;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace SpineHelper.Export
 {
@@ -15,88 +17,92 @@ namespace SpineHelper.Export
 
         public virtual string ExportDataToFile(object[,] dataTable, string filename)
         {
-
-            _Application excel = new Application();
-            excel.StandardFont = "Calibri";
-            excel.StandardFontSize = 11;
-            _Workbook workbook = excel.Workbooks.Add(Type.Missing);
-            _Worksheet worksheet = null;
-
-            try
+            using (var p = new ExcelPackage())
             {
-                worksheet = workbook.ActiveSheet;
-                worksheet.Name = WorksheetName;
-                worksheet.Columns.ColumnWidth = 6;
-
-
-                int xOffset = 2; //Should be at least 1 (excel starts at (1,1)
-                int yOffset = 2; //Should be at least 1 (excel starts at (1,1)
-
-                int numColumns = dataTable.GetLength(0);
-                int numRows = dataTable.GetLength(1);
-
-                var firstRow = worksheet.Range[worksheet.Cells[yOffset, xOffset], worksheet.Cells[yOffset, numColumns - 1 + xOffset]];
-                firstRow.Merge(true);
-                firstRow.HorizontalAlignment = XlHAlign.xlHAlignCenter;
-                firstRow.Interior.Color = XlRgbColor.rgbPowderBlue;
-                firstRow.Font.Bold = true;
-                worksheet.Cells[yOffset++, xOffset] = WorksheetName;
-
-                var secondRow = worksheet.Range[worksheet.Cells[yOffset, xOffset], worksheet.Cells[yOffset, numColumns - 1 + xOffset]];
-                secondRow.Merge(true);
-                secondRow.HorizontalAlignment = XlHAlign.xlHAlignCenter;
-
-                yOffset++;
-
-                // Format Headers
-                for (int column = 0; column < numColumns; column++)
+                try
                 {
-                    worksheet.Columns[column + xOffset].NumberFormat = GetFormattingByColumnName(dataTable[column, 0]);
-                    worksheet.Columns[column + xOffset].ColumnWidth = GetColumnWidthByColumnName(dataTable[column, 0]);
+                    var worksheet = p.Workbook.Worksheets.Add(WorksheetName);
 
-                    worksheet.Cells[yOffset, column + xOffset].HorizontalAlignment = XlHAlign.xlHAlignCenter;
-                    worksheet.Cells[yOffset, column + xOffset].Interior.Color = XlRgbColor.rgbBrown;
-                    worksheet.Cells[yOffset, column + xOffset].Font.Color = XlRgbColor.rgbWhite;
+                    // Set default font
+                    /*
+                    var allCells = worksheet.Cells[1, 1, worksheet.Dimension.End.Row, worksheet.Dimension.End.Column];
+                    var cellFont = allCells.Style.Font;
+                    cellFont.SetFromFont(new Font("Calibri", 11));
+                    */
 
-                    worksheet.Cells[yOffset, column + xOffset].Font.Name = "Arial";
-                }
+                    // Set default column size
+                    for (int i = 1; i <= 20; i++)
+                        worksheet.Column(i).Width = 6.6;
 
-                // Copy data
-                for (int row = 0; row < numRows; row++)
-                {
+
+                    int xOffset = 2; //Should be at least 1 (excel starts at (1,1)
+                    int yOffset = 2; //Should be at least 1 (excel starts at (1,1)
+
+                    int numColumns = dataTable.GetLength(0);
+                    int numRows = dataTable.GetLength(1);
+
+                    var firstRow = worksheet.Cells[yOffset, xOffset, yOffset, numColumns - 1 + xOffset];
+                    firstRow.Merge = true;
+                    firstRow.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    firstRow.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    firstRow.Style.Fill.BackgroundColor.SetColor(Color.PowderBlue);  //rgbPowderBlue
+                    firstRow.Style.Font.Bold = true;
+
+
+                    worksheet.Cells[yOffset++, xOffset].Value = WorksheetName;
+
+
+                    var secondRow = worksheet.Cells[yOffset, xOffset, yOffset, numColumns - 1 + xOffset];
+                    secondRow.Merge = true;
+                    secondRow.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                    yOffset++;
+
+
+                    // Format Headers
                     for (int column = 0; column < numColumns; column++)
                     {
-                        var obj = dataTable[column, row];
-                        //var cell = worksheet.Cells[row + yOffset, column + xOffset];
+                        worksheet.Column(column + xOffset).Style.Numberformat.Format = GetFormattingByColumnName(dataTable[column, 0]);
+                        worksheet.Column(column + xOffset).Width = GetColumnWidthByColumnName(dataTable[column, 0]);
 
-                        if (obj != null)
-                            worksheet.Cells[row + yOffset, column + xOffset] = obj;
+                        worksheet.Cells[yOffset, column + xOffset].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        worksheet.Cells[yOffset, column + xOffset].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        worksheet.Cells[yOffset, column + xOffset].Style.Fill.BackgroundColor.SetColor(Color.FromArgb(154, 34, 34)); //rgbBrown
+                        worksheet.Cells[yOffset, column + xOffset].Style.Font.Color.SetColor(Color.White);
+                        worksheet.Cells[yOffset, column + xOffset].Style.Font.Name = "Arial";
+                    }
 
-                        // Summary rows
-                        if (row >= numRows - 2 && obj != null)
+                    // Copy data
+                    for (int row = 0; row < numRows; row++)
+                    {
+                        for (int column = 0; column < numColumns; column++)
                         {
-                            worksheet.Cells[row + yOffset, column + xOffset].Interior.Color = Color.FromArgb(215, 228, 188);
-                            worksheet.Cells[row + yOffset, column + xOffset].HorizontalAlignment = XlHAlign.xlHAlignRight;
-                            if (row < numRows - 1)
-                                worksheet.Cells[row + yOffset, column + xOffset].Font.Bold = true;
+                            var obj = dataTable[column, row];
+                            //var cell = worksheet.Cells[row + yOffset, column + xOffset];
+
+                            if (obj != null)
+                                worksheet.Cells[row + yOffset, column + xOffset].Value = obj;
+
+                            // Summary rows
+                            if (row >= numRows - 2 && obj != null)
+                            {
+                                worksheet.Cells[row + yOffset, column + xOffset].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                                worksheet.Cells[row + yOffset, column + xOffset].Style.Fill.BackgroundColor.SetColor(Color.FromArgb(215, 228, 188));
+                                worksheet.Cells[row + yOffset, column + xOffset].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                                if (row < numRows - 1)
+                                    worksheet.Cells[row + yOffset, column + xOffset].Style.Font.Bold = true;
+                            }
                         }
                     }
+
+                    p.SaveAs(new FileInfo(filename));
+                    return null;
+
                 }
-
-                // Summary rows end
-
-                workbook.SaveAs(filename);
-                return null;
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
-            finally
-            {
-                excel.Quit();
-                workbook = null;
-                excel = null;
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
             }
         }
 
@@ -118,34 +124,24 @@ namespace SpineHelper.Export
 
         private double GetColumnWidthByColumnName(object columnData)
         {
-            if (columnData == null) return 8;
+            if (columnData == null) return 8.6;
 
             String name = columnData.ToString();
 
-            if (name == "ID") return 2;
-            if (name == "ASTM") return 8.5;
-            if (name == "AMO") return 6.8;
-            if (name == GlobalStrings.HistoryViewColumnGrains) return 8;
-            if (name == GlobalStrings.HistoryViewColumnGrams) return 8.5;
-            if (name == GlobalStrings.HistoryViewColumnStraightness) return 11;
-            if (name == GlobalStrings.HistoryViewColumnComment) return 25;
-            if (name == GlobalStrings.HistoryViewColumnGPI) return 7;
+            if (name == "ID") return 2.6;
+            if (name == "ASTM") return 9.1;
+            if (name == "AMO") return 7.4;
+            if (name == GlobalStrings.HistoryViewColumnGrains) return 8.6;
+            if (name == GlobalStrings.HistoryViewColumnGrams) return 9.1;
+            if (name == GlobalStrings.HistoryViewColumnStraightness) return 11.6;
+            if (name == GlobalStrings.HistoryViewColumnComment) return 25.6;
+            if (name == GlobalStrings.HistoryViewColumnGPI) return 7.6;
 
-            return 10;
+            return 10.6;
         }
 
 
-        private void CreateTestColorPalette(_Worksheet worksheet, int startX, int startY)
-        {
-            // Color test
-            int i = 0;
-            foreach (var item in Enum.GetNames(typeof(XlRgbColor)))
-            {
-                worksheet.Cells[i + startY, startX].Interior.Color = (XlRgbColor)Enum.Parse(typeof(XlRgbColor), item);
-                worksheet.Cells[i + startY, startX + 1] = item;
-                i++;
-            }
-        }
+
     }
 
 
